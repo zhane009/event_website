239ade536e63559e1377850d52bcea6dc776e5f0
Lin Wai Phyo <linwaiphyo.akp@gmail.com>
2023-11-08 23:52:19 +0000

    initial commit

---
+++ script/home.js
@@ -0,0 +1,12 @@
+function responsiveNav() {
+    var x = document.querySelector(".navbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+}
+
+var icon = document.querySelector(".icon");
+icon.addEventListener("click", responsiveNav);
+

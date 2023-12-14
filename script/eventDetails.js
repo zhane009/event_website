acb03d5e88c75ea5d75092bebd463ce8015adc92
Lin Wai Phyo <linwaiphyo.akp@gmail.com>
2023-12-14 01:33:47 +0000

    Event details page layout

--- script/eventDetails.js
+++ script/eventDetails.js
@@ -1,4 +1,3 @@
-
 function responsiveNav() {
     var x = document.querySelector(".navbar");
     if (x.className === "navbar") {
@@ -20,14 +19,18 @@ darkMode.addEventListener("click", function() {
 
       document.querySelector("#dark-mode a").setAttribute("class", "fa-solid fa-sun");
       localStorage.darkMode = false;
+      document.querySelector(".details").setAttribute("style", "background-color: white;");
+      document.querySelector(".desc").setAttribute("style", "background-color: white;");
     } 
 
     else {
 
       document.querySelector("#dark-mode a").setAttribute("class", "fa-solid fa-moon");
       localStorage.darkMode = true;
+      document.querySelector(".details").setAttribute("style", "background-color: #333; color: gainsboro;");
+      document.querySelector(".desc").setAttribute("style", "background-color: #333; color: gainsboro;");
+    } 
     }
-}
 );	
 
 var control = document.querySelector("#controlButton");
@@ -100,9 +103,14 @@ fontReset.addEventListener("click", function() {
 if(localStorage.darkMode == "false") {
   document.body.classList.add("dark");
   document.querySelector("#dark-mode a").setAttribute("class", "fa-solid fa-sun");
+  document.querySelector(".details").setAttribute("style", "background-color: white;");
+  document.querySelector(".desc").setAttribute("style", "background-color: white;");
+
 }
 else {
   document.querySelector("#dark-mode a").setAttribute("class", "fa-solid fa-moon");
+  document.querySelector(".details").setAttribute("style", "background-color: #333; color: gainsboro;");
+  document.querySelector(".desc").setAttribute("style", "background-color: #333; color: gainsboro;");
 }
 
 if (localStorage.fontSize == "x-large"){
@@ -114,103 +122,3 @@ else if (localStorage.fontSize == "medium"){
 else {
   document.body.style.fontSize = "large";
 }
-
-
-function getEvents() {
-  return fetch("./data/events.json")
-  .then(response => response.json())
-};
-
-getEvents().then(data => {
-  var events = data.details;
-  // var i = 0;
-  var index = 0;
-
-  for (let event in events){
-    
-    if (index >= events.length){
-      break;
-    }
-
-    const eventArea = document.querySelector(".eventbox .events");
-    const row = document.createElement("div");
-    row.classList.add("event-row");
-
-    do {
-      if (index >= events.length){
-        break;
-      }
-      var id = "event" + (index + 1);
-      const container = document.createElement("a");
-      container.href = "eventDetails.html";
-
-      const div = document.createElement("div");
-      div.classList.add("event");
-      div.id = id;
-
-      const img = document.createElement("img");
-      img.classList.add("eventImage");
-      img.src = events[index].image;
-
-      const overlay = document.createElement("div");
-      overlay.classList.add("overlay");
-
-      const overlay2 = document.createElement("div");
-      overlay2.classList.add("overlay-text");
-
-      const title = document.createElement("h3");
-      title.classList.add("eventTitle");
-      title.innerHTML = events[index].title;
-
-      const desc = document.createElement("p");
-      desc.classList.add("eventDesc");
-      desc.innerHTML = events[index].description; 
-
-      const location = document.createElement("p");
-      location.innerHTML = events[index].location;
-      location.classList.add("eventDesc");
-
-      const date = document.createElement("p");
-      date.innerHTML = events[index].date;
-      date.classList.add("eventDesc");
-
-      
-
-      div.appendChild(img);
-      overlay2.appendChild(title);
-      overlay2.appendChild(desc);
-      overlay2.appendChild(location);
-      overlay2.appendChild(date);
-
-      // overlay.appendChild(button);
-      div.appendChild(overlay);
-      div.appendChild(overlay2);
-      container.appendChild(div);
-      row.appendChild(container)
-
-      index += 1;
-
-    }while (index % 2 != 0);
-
-    eventArea.appendChild(row);
-
-    // i++;
-    if (index > events.length){
-      break;
-    }
-
-  }
-
-});
-
-
-var scrollBtn = document.querySelector("#scroll");
-
-scrollBtn.addEventListener("click", function() {
-  var events = document.querySelector(".eventbox");
-    events.scrollIntoView({
-        behavior: "smooth",
-        alignTop: true
-    });
-});
-
